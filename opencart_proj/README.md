bfe5d02b97c7af2beec7498188b88439fa7d40f3
zhangrj <zhang_career@foxmail.com>
2023-03-05 13:34:27 +0800

    opencart added

---
+++ opencart_proj/README.md
@@ -0,0 +1,4 @@
+
+# Checkage
+## PHP
+http://127.0.0.1:8000/info.php
---
+++ opencart_proj/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rm -rfv var/nginx
+rm -rfv var/php-fpm
+rm -rfv var/mysql
+rm -rfv var/postgres
+rm -rfv var/redis
+rm -rfv var/opencart
